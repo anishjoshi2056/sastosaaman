2921b0fa99cec4c4121960fb569ccc2d176f8b24
Anish Joshi <anishjoshi2056@hotmail.com>
2020-07-12 09:49:11 +0545

    starter code

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverrirde = require('method-override');
+const passport = require('passport');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect('mongodb://localhost:27017/secondhand',{useNewUrlParser:true,useUnifiedTopology:true});
+
+app.get('/',(req,res)=>{
+    res.send('hi there !!');
+})
+
+
+app.listen('3000',()=> {
+    console.log('server started');
+})
