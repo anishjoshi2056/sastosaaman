f2172b3b7d96c0404f0b6679b6399cad5e627a62
Anish Joshi <anishjoshi2056@hotmail.com>
2020-07-12 19:27:36 +0545

    added google oauth successfully

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverrirde = require('method-override');
 const passport = require('passport');
+const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
@@ -11,11 +13,25 @@ app.use(express.static(__dirname + "/public"));
 
 mongoose.connect('mongodb://localhost:27017/secondhand',{useNewUrlParser:true,useUnifiedTopology:true});
 
+//Passport configuration
+app.use(require('express-session')({
+    secret:"I love you",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth',authRoutes);
 //homepage route
 app.get('/',(req,res)=>{
     res.render('homepage')
 })
 
+app.get('/items',function(req,res) {
+    res.render('secondhand/index');
+})
 
 app.listen('3000',()=> {
     console.log('server started');
