395ca1cdbddc6e15cc0f24ebe538cd1e61469e9c
Anish Joshi <anishjoshi2056@hotmail.com>
2020-07-30 16:57:56 +0545

    successfully added a show.ejs page

--- routes/saamaan-routes.js
+++ routes/saamaan-routes.js
@@ -13,4 +13,46 @@ router.get('/',function(req,res){
         }
     })
 })
+//Route for a new form(new.ejs)
+router.get('/new',authCheck,function(req,res){
+    //Rendering new.ejs(to get a form)
+    res.render('secondhand/new');
+})
+router.post('/',authCheck,function(req,res){
+    var name = req.body.name;
+    var img = req.body.img;
+    var desc = req.body.desc;
+    var price = req.body.price;
+    var author = {
+        id:req.user._id,
+        username:req.user.username
+    }
+    var newSaamaan = {
+        name:name,
+        img:img,
+        desc:desc,
+        price:price,
+        author:author
+    }
+    //Create a new campground and save into the database
+    Saamaan.create(newSaamaan,function(err,newlySaamaan){
+        if(err){
+            console.log(err);
+        }else{
+            //redirect back to default index page
+            res.redirect('/saamaan');
+        }
+    })
+})
+// For more information
+router.get('/:id',function(req,res){
+    Saamaan.findById(req.params.id,function(err,foundSaamaan){
+        if(err){
+            console.log(err);
+        }else {
+            res.render('secondhand/show',{foundSaamaan:foundSaamaan})
+        }
+    })
+    
+})
 module.exports = router;
